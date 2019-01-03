518324836ea51620b396bff9782bd33b325c248f
dhoonjang <dhoonjang@gmail.com>
2019-01-03 20:38:55 +0900

    1.31

--- src/api/Hello/sayBye/sayBye.resolvers.ts
+++
@@ -1,7 +0,0 @@
-const resolvers = {
-    Query: {
-        sayBye: () => "Good Bye!"
-    }
-};
-
-export default resolvers;
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -7,9 +7,16 @@ import {
     Column, 
     CreateDateColumn, 
     Entity,
+    ManyToOne,
+    OneToMany,
     PrimaryGeneratedColumn, 
     UpdateDateColumn
 } from 'typeorm';
+import Chat from './Chat';
+import Message from './Message';
+import Ride from './Ride';
+import Verification from './Verification';
+
 
 const BCRYPT_ROUNDS = 10;
 
@@ -17,9 +24,9 @@ const BCRYPT_ROUNDS = 10;
 class User extends BaseEntity {
     @PrimaryGeneratedColumn() id: number;
 
-    @Column({type: "text", unique: true})
+    @Column({type: "text", nullable: true})
     @IsEmail()
-    email: string;
+    email: string | null;
 
     @Column({type: "boolean", default: false})
     verifiedEmail: boolean;
@@ -30,13 +37,13 @@ class User extends BaseEntity {
     @Column({ type: "text" })
     lastName: string;
   
-    @Column({ type: "int" })
+    @Column({ type: "int", nullable: true })
     age: number;
   
-    @Column({ type: "text" })
+    @Column({ type: "text", nullable: true })
     password: string;
   
-    @Column({ type: "text" })
+    @Column({ type: "text", nullable: true })
     phoneNumber: string;
   
     @Column({ type: "boolean", default: false })
@@ -63,7 +70,26 @@ class User extends BaseEntity {
     @Column({ type: "double precision", default: 0 })
     lastOrientation: number;
 
+    @Column({ type: "text", nullable: true})
+    fbId: string;
+
+    @ManyToOne(type => Chat, chat => chat.participants)
+    chat: Chat;
+
+    @OneToMany(type => Message, message => message.user)
+    messages: Message[];
+
+    @OneToMany(type => Verification, verification => verification.user)
+    verifications: Verification[];
+
+    @OneToMany(type => Ride, ride => ride.passenger)
+    ridesAsPassenger: Ride[];
+
+    @OneToMany(type => Ride, ride => ride.driver)
+    ridesAsDriver: Ride[];
+
     @CreateDateColumn() createdAt: string;
+
     @UpdateDateColumn() updatedAt: string;
 
     get fullName(): string {
@@ -73,7 +99,7 @@ class User extends BaseEntity {
     public comparePassword(password: string): Promise<boolean> {
         return bcrypt.compare(password, this.password);
     }
-    
+
     @BeforeInsert()
     @BeforeUpdate()
     async savePassword(): Promise<void> {
